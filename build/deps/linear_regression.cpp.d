43abdcfa4a691a8df418336d712cf0dbab845e8b
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-07-21 13:39:40 +0200

    added .gitignore

--- build/deps/linear_regression.cpp.d
+++
@@ -1,3 +0,0 @@
-build/obj/linear_regression.cpp.RELEASE.o: src/linear_regression.cpp \
- include/linear_regression.hpp
-include/linear_regression.hpp:
--- build/deps/main.cpp.d
+++
@@ -1,2 +0,0 @@
-build/obj/main.cpp.RELEASE.o: src/main.cpp include/linear_regression.hpp
-include/linear_regression.hpp:
