530c22894386c8498c81bbe470caffb4a8ae0976
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-07-22 22:21:46 +0200

    added threading to parts of the code

--- src/linear_regression.cpp
+++ src/linear_regression.cpp
@@ -1,27 +1,39 @@
 #include "linear_regression.hpp"
-#include "vector_ops.hpp"
+#include "helper_ops.hpp"
 #include <cmath>
 
 using namespace ML;
 
 LinearRegression::LinearRegression(const double &alpha) : alpha(alpha) {}
 
+static void initialize(const size_t &i, vector<vector<data>> &X,
+                       const vector<vector<data>> &x, const size_t &features) {
+  for (size_t j = 0; j < features; j++)
+    X[j][i] = x[i][j];
+}
+
+static void getStats(const size_t &i, vector<data> &avg,
+                     vector<vector<data>> &X, vector<data> &std_dev,
+                     const size_t &data_size) {
+  avg[i] = add(X[i]) / data_size;
+  add(X[i], -avg[i]);
+  std_dev[i] = sqrt(dotProduct(X[i], X[i]) / data_size);
+  mul(X[i], 1 / std_dev[i]);
+}
+
 vector<vector<data>>
 LinearRegression::transform(const vector<vector<data>> &x) {
   size_t features = x[0].size(), data_size = x.size();
   vector<vector<data>> X(features, vector<data>(data_size));
-  for (size_t i = 0; i < data_size; i++)
-    for (size_t j = 0; j < features; j++)
-      X[j][i] = x[i][j];
+
+  runLoopConcurrently(0, data_size, initialize, std::ref(X), std::cref(x),
+                      std::cref(features));
 
   avg.resize(features);
   std_dev.resize(features);
-  for (size_t i = 0; i < features; i++) {
-    avg[i] = add(X[i]) / data_size;
-    add(X[i], -avg[i]);
-    std_dev[i] = sqrt(dotProduct(X[i], X[i]) / data_size);
-    mul(X[i], 1 / std_dev[i]);
-  }
+
+  runLoopConcurrently(0, features, getStats, std::ref(avg), std::ref(X),
+                      std::ref(std_dev), std::cref(data_size));
 
   return X;
 }
@@ -65,28 +77,33 @@ bool LinearRegression::processData(const vector<vector<data>> &dotX,
   return true;
 }
 
+static void precalculate(const size_t &i, const size_t &features,
+                         const vector<vector<data>> &X, const vector<data> &y,
+                         vector<data> &sumX, vector<data> &sumXY,
+                         vector<vector<data>> &dotX, const data &multiplicand) {
+  sumX[i] = add(X[i]);
+  sumXY[i] = dotProduct(X[i], y);
+
+  for (size_t j = i; j < features; j++)
+    dotX[i][j] = dotProduct(X[i], X[j]);
+
+  mul(dotX[i], multiplicand);
+}
+
 bool LinearRegression::train(const vector<vector<data>> &x,
                              const vector<data> &y) {
   auto X = transform(x);
   size_t features = X.size(), data_size = y.size();
 
-  vector<vector<data>> dotX(features);
-  vector<data> sumX, sumXY;
-  sumX.reserve(features);
-  sumXY.reserve(features);
+  vector<vector<data>> dotX(features, vector<data>(features));
+  vector<data> sumX(features), sumXY(features);
 
-  for (size_t i = 0; i < features; i++) {
-    sumX.emplace_back(add(X[i]));
-    sumXY.emplace_back(dotProduct(X[i], y));
-
-    dotX[i].reserve(features);
+  runLoopConcurrently(0, features, precalculate, std::cref(features),
+                      std::cref(X), std::cref(y), std::ref(sumX),
+                      std::ref(sumXY), std::ref(dotX), alpha / data_size);
+  for (size_t i = 0; i < features; i++)
     for (size_t j = 0; j < i; j++)
-      dotX[i].emplace_back(dotX[j][i]);
-    for (size_t j = i; j < features; j++)
-      dotX[i].emplace_back(dotProduct(X[i], X[j]));
-
-    mul(dotX[i], alpha / data_size);
-  }
+      dotX[i][j] = dotX[j][i];
 
   mul(sumX, alpha / data_size);
   mul(sumXY, alpha / data_size);
