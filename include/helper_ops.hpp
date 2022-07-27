bb91ccedb8828e8dc1a3cc831e470e38789c1842
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-07-27 14:04:40 +0200

    fixed an error with the namespace

--- include/helper_ops.hpp
+++ include/helper_ops.hpp
@@ -27,6 +27,6 @@ void mul(vector<data> &a, data value);
 data add(const vector<data> &a);
 void add(vector<data> &a, data value);
 
-#include "helper_ops.tpp"
-
 } // namespace ML
+
+#include "helper_ops.tpp"
