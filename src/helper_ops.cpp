6d3306987e03ef6e76ae154ccbf384157e4ddc8e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-07-27 20:58:43 +0200

    added scale to dotProduct and sum

--- src/helper_ops.cpp
+++ src/helper_ops.cpp
@@ -9,20 +9,21 @@ void ML::mul(vector<data> &a, data value) {
     a[i] *= value;
 }
 
-data ML::dotProduct(const vector<data> &a, const vector<data> &b) {
+data ML::dotProduct(const vector<data> &a, const vector<data> &b,
+                    const data scale) {
   size_t n = a.size();
   assert(n == b.size());
   data result = 0;
   for (size_t i = 0; i < n; i++)
-    result += a[i] * b[i];
+    result += scale * a[i] * b[i];
   return result;
 }
 
-data ML::add(const vector<data> &a) {
+data ML::sum(const vector<data> &a, const data scale) {
   size_t n = a.size();
   data result = 0;
   for (size_t i = 0; i < n; i++)
-    result += a[i];
+    result += a[i] * scale;
   return result;
 }
 
