62d68f30a5f2548f77c472d03b394509fa9c4722
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-08-02 01:51:03 +0200

    better naming and readability (lambda functions)

--- include/Helper_Operations/vector_operations.hpp
+++ include/Helper_Operations/vector_operations.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Helper_Operations/defaults.hpp"
+#include "defaults.hpp"
 
 namespace MachineLearning {
 
--- src/Helper_Operations/vector_operations.cpp
+++ src/Helper_Operations/vector_operations.cpp
@@ -1,36 +1,45 @@
 #include "Helper_Operations/vector_operations.hpp"
+#include "Helper_Operations/concurrency_operations.hpp"
 #include <cassert>
+#include <stdexcept>
 
 using namespace MachineLearning;
 
-void MachineLearning::multiplyByScalar(vector<DataType> &a, DataType value) {
-  size_t n = a.size();
-  for (size_t i = 0; i < n; i++)
-    a[i] *= value;
+static const size_t maxIterationsPerThread = 1e7;
+
+void MachineLearning::multiplyByScalar(vector<DataType> &vec, DataType value) {
+  size_t size = vec.size();
+  ConcurrentLoops driver(0, size, maxIterationsPerThread);
+  auto loopBody = [](size_t i, vector<DataType> &vec,
+                     const DataType &value) -> void { vec[i] *= value; };
+  driver.initiateLoopsWithoutReturns(loopBody, std::ref(vec), std::cref(value));
 }
 
-DataType MachineLearning::dotProduct(const vector<DataType> &a,
-                                     const vector<DataType> &b,
+DataType MachineLearning::dotProduct(const vector<DataType> &vec1,
+                                     const vector<DataType> &vec2,
                                      const DataType scale) {
-  size_t n = a.size();
-  assert(n == b.size());
+  size_t size = vec1.size();
+  if (size != vec2.size())
+    throw std::invalid_argument("Vector shapes do not match");
   DataType result = 0;
-  for (size_t i = 0; i < n; i++)
-    result += scale * a[i] * b[i];
+  for (size_t i = 0; i < size; i++)
+    result += scale * vec1[i] * vec2[i];
   return result;
 }
 
-DataType MachineLearning::getSummation(const vector<DataType> &a,
+DataType MachineLearning::getSummation(const vector<DataType> &vec,
                                        const DataType scale) {
-  size_t n = a.size();
+  size_t size = vec.size();
   DataType result = 0;
-  for (size_t i = 0; i < n; i++)
-    result += a[i] * scale;
+  for (size_t i = 0; i < size; i++)
+    result += vec[i] * scale;
   return result;
 }
 
-void MachineLearning::increaseByScalar(vector<DataType> &a, DataType value) {
-  size_t n = a.size();
-  for (size_t i = 0; i < n; i++)
-    a[i] += value;
+void MachineLearning::increaseByScalar(vector<DataType> &vec, DataType value) {
+  size_t size = vec.size();
+  ConcurrentLoops driver(0, size, maxIterationsPerThread);
+  auto loopBody = [](size_t i, vector<DataType> &vec,
+                     const DataType &value) -> void { vec[i] += value; };
+  driver.initiateLoopsWithoutReturns(loopBody, std::ref(vec), std::cref(value));
 }
