f36525189cf97089006dac737ba6598653bb83ff
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-08-02 18:49:51 +0200

    removed assert and concurrency

--- src/Helper_Operations/vector_operations.cpp
+++ src/Helper_Operations/vector_operations.cpp
@@ -1,6 +1,4 @@
 #include "Helper_Operations/vector_operations.hpp"
-#include "Helper_Operations/concurrency_operations.hpp"
-#include <cassert>
 #include <stdexcept>
 
 using namespace MachineLearning;
@@ -8,9 +6,9 @@ using namespace MachineLearning;
 static const size_t maxIterationsPerThread = 1e7;
 
 void MachineLearning::multiplyByScalar(vector<DataType> &vec, DataType value) {
-  ConcurrentLoops driver(0, vec.size(), maxIterationsPerThread);
-  driver.initiateLoopsWithoutReturns(
-      [&](size_t i) -> void { vec[i] *= value; });
+  size_t size = vec.size();
+  for (size_t i = 0; i < size; i++)
+    vec[i] *= value;
 }
 
 DataType MachineLearning::dotProduct(const vector<DataType> &vec1,
@@ -19,6 +17,7 @@ DataType MachineLearning::dotProduct(const vector<DataType> &vec1,
   size_t size = vec1.size();
   if (size != vec2.size())
     throw std::invalid_argument("Vector shapes do not match");
+
   DataType result = 0;
   for (size_t i = 0; i < size; i++)
     result += scale * vec1[i] * vec2[i];
@@ -30,12 +29,12 @@ DataType MachineLearning::getSummation(const vector<DataType> &vec,
   size_t size = vec.size();
   DataType result = 0;
   for (size_t i = 0; i < size; i++)
-    result += vec[i] * scale;
+    result += scale * vec[i];
   return result;
 }
 
 void MachineLearning::increaseByScalar(vector<DataType> &vec, DataType value) {
-  ConcurrentLoops driver(0, vec.size(), maxIterationsPerThread);
-  driver.initiateLoopsWithoutReturns(
-      [&](size_t i) -> void { vec[i] += value; });
+  size_t size = vec.size();
+  for (size_t i = 0; i < size; i++)
+    vec[i] += value;
 }
